5a72cbe2eba132580a04a722211bfc7583659c16
Adam White <arakaska@gmail.com>
2015-10-15 08:38:19 -0600

    Added Range class

--- Thinq/ThinqCore/Range.cs
+++ Thinq/ThinqCore/Range.cs
@@ -20,9 +20,62 @@ using System.Reactive.Linq.Observαble;
 
 namespace ThinqCore
 {
-	public static class Range
-	{
-			
+	public class Range
+	{		
+		private int _min;
+		private int _max;
+		private int _step;
+		private int _length;
+		private int _cursorPosition;
 
-	}
+		public Range(int min, int max, int stepSize, int length)
+		{			
+			if (max < 1) { throw new ArgumentOutOfRangeException("max"); }
+			if (length < 0) { throw new ArgumentOutOfRangeException("length"); }
+			if (stepSize < 1) { throw new ArgumentOutOfRangeException("root"); }
+			
+			_min = min;
+			_max = max;
+			_step = stepSize;
+			_length = length;
+			_cursorPosition = 0;
+		}
+
+		public IEnumerable<int> GetNext()
+		{
+			while (_cursorPosition < _max)
+			{
+				_cursorPosition += _step;
+				yield return _cursorPosition;
+			}
+		}
+
+		public bool Contains(int value)
+		{
+			if (value < _min)
+			{
+				return false;
+			}
+			if (value > _max)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public override bool Equals(object obj)
+		{
+			bool result = false;
+			Range other = obj as Range;
+			if (other != null)
+			{
+				result = this._min == other._min;
+				result &= this._max == other._max;
+				result &= this._step == other._step;
+				result &= this._length == other._length;
+			}
+			return result;
+		}
+
+	} // END class
 }
