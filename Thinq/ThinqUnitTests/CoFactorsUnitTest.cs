db5ba0be2f10ec121cee097554a8ea44af54ebc7
Adam White <arakaska@gmail.com>
2016-04-07 03:02:22 -0600

    Added UnitTest Project

---
+++ Thinq/ThinqUnitTests/CoFactorsUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ThinqCore;
+
+namespace ThinqUnitTests
+{
+	[TestClass]
+	public class CoFactorsUnitTest
+	{
+		[TestMethod]
+		public void TestCoFactorsExplicit()
+		{
+			ulong minValue = 800100100;
+			ulong maxValue = 900000000;
+			ulong[] cofactors = new ulong[] { 3, 7, 11, 13, 17, 19, 23 };
+			int expectedFactorCount = 5;
+			List<ulong> expectedValues = new List<ulong>() { 803134332, 825443619, 847752906, 870062193, 892371480 };
+
+			Intersection intersection = new Intersection(minValue, maxValue, cofactors);
+			List<ulong> intersectionResult = intersection.GetEnumerable().ToList();
+			int resultFactorCount = intersectionResult.Count;
+
+			Assert.AreEqual(expectedFactorCount, resultFactorCount);
+			Assert.IsFalse(expectedValues.Except(intersectionResult).Any());
+		}
+
+		[TestMethod]
+		public void TestCoFactorsCount()
+		{
+			ulong minValue = 4300100100;
+			ulong maxValue = 9000000000;
+			ulong[] cofactors = new ulong[] { 251, 257, 259, 263 };
+			int expectedFactorCount = 2;
+			ulong expectedFirst = 4394023319;
+			ulong expectedLast = 8788046638;
+
+			Intersection intersection = new Intersection(minValue, maxValue, cofactors);
+			List<ulong> intersectionResult = intersection.GetEnumerable().ToList();
+			int resultFactorCount = intersectionResult.Count;
+
+			Assert.AreEqual(expectedFactorCount, resultFactorCount);
+			Assert.AreEqual(expectedFirst, intersectionResult.First());
+			Assert.AreEqual(expectedLast, intersectionResult.Last());
+		}
+
+		[TestMethod]
+		public void TestCoFactorsEmpty()
+		{
+			ulong minValue = 4200100100;
+			ulong maxValue = 8300100100;
+			ulong[] cofactors = new ulong[] { 251, 257, 64506 };
+			int expectedFactorCount = 0;
+
+			Intersection intersection = new Intersection(minValue, maxValue, cofactors);
+			List<ulong> intersectionResult = intersection.GetEnumerable().ToList();
+			int resultFactorCount = intersectionResult.Count;
+
+			Assert.AreEqual(expectedFactorCount, resultFactorCount);
+		}
+	}
+}
---
+++ Thinq/ThinqUnitTests/CoPrimesUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ThinqCore;
+
+namespace ThinqUnitTests
+{
+	[TestClass]
+	public class CoPrimesUnitTest
+	{
+		[TestMethod]
+		public void TestCoPrimes256()
+		{
+			int coprimeTo = 256;
+			int coprimeMin = 16777216;
+			int coprimeMax = 16777700;
+			int expectedCoprimeCount = 242;
+			int expectedFirst = 16777217;
+			int expectedLast = 16777699;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<int> resultCoprimes = coprimes.GetCoprimes().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+			Assert.AreEqual(expectedFirst, resultCoprimes.First());
+			Assert.AreEqual(expectedLast, resultCoprimes.Last());
+		}
+
+		[TestMethod]
+		public void TestCoPrimes253()
+		{
+			int coprimeTo = 253;
+			int coprimeMin = 1456777000;
+			int coprimeMax = 1456777777;
+			int expectedCoprimeCount = 675;
+			int expectedFirst = 1456777000;
+			int expectedLast = 1456777776;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<int> resultCoprimes = coprimes.GetCoprimes().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+			Assert.AreEqual(expectedFirst, resultCoprimes.First());
+			Assert.AreEqual(expectedLast, resultCoprimes.Last());
+		}
+
+		[TestMethod]
+		public void TestCoPrimes251()
+		{
+			int coprimeTo = 251;
+			int coprimeMin = 1234567000;
+			int coprimeMax = 1234568000;
+			int expectedCoprimeCount = 996;
+			int expectedFirst = 1234567000;
+			int expectedLast = 1234567999;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<int> resultCoprimes = coprimes.GetCoprimes().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+			Assert.AreEqual(expectedFirst, resultCoprimes.First());
+			Assert.AreEqual(expectedLast, resultCoprimes.Last());
+		}
+
+		[TestMethod]
+		public void TestCoPrimesEmpty()
+		{
+			int coprimeTo = 20160;
+			int coprimeMin = 6777122;
+			int coprimeMax = 6777131;
+			int expectedCoprimeCount = 0;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<int> resultCoprimes = coprimes.GetCoprimes().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+		}
+	}
+}
