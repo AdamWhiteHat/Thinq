5f0b9d944da98253fa3671cf46701f4e7e654d83
Adam White <arakaska@gmail.com>
2016-04-06 23:30:56 -0600

    Added some XML comments, re-arranged members

--- ThinqGUI/AsyncBackgroundTask.cs
+++ ThinqGUI/AsyncBackgroundTask.cs
@@ -11,70 +11,131 @@ namespace ThinqGUI
 {
 	public class AsyncBackgroundTask : IDisposable
 	{
-		BackgroundWorker _backgroundWorker;
+		#region Public Properties & Events
 
-		internal Stats ArithmeticSequenceStats { get; private set; }
-		public Intersection IntersectionSet { get; private set; }
-
-		public bool IsDisposed { get; private set; }
+		/// <summary>
+		/// Gets a value indicating whether the AsyncBackgroundTask is running an asynchronous operation.
+		/// </summary>
 		public bool IsBusy { get; private set; }
-		public bool CancellationPending { get; private set; }
-		public bool WorkerReportsProgress { get { return true; } }
-		public bool WorkerSupportsCancellation { get { return true; } }
 
-		public event DoWorkEventHandler DoWork 
+		/// <summary>
+		/// Gets a value indicating whether the Dispose() method has been called.
+		/// </summary>
+		public bool IsDisposed { get; private set; }
+
+		/// <summary>
+		/// Gets a value indicating whether the application has requested cancellation of a background operation.
+		/// </summary>
+		public bool CancellationPending { get; private set; }
+
+		/// <summary>
+		/// Gets a value indicating whether the AsyncBackgroundTask class supports cancellation of a background operation. This value always returns true.
+		/// </summary>
+		public bool WorkerSupportsCancellation { get { return true; } }
+		
+		/// <summary>
+		/// Occurs when AsyncBackgroundTask.RunWorkerAsync() is called.
+		/// </summary>		
+		public event DoWorkEventHandler DoWork
 		{
 			add { if (!IsDisposed) { _backgroundWorker.DoWork += value; } }
 			remove { if (!IsDisposed) { _backgroundWorker.DoWork -= value; } }
 		}
 
+		/// <summary>
+		/// Occurs when the background operation has completed, has been canceled, or has raised an exception.
+		/// </summary>
 		public event RunWorkerCompletedEventHandler RunWorkerCompleted
 		{
 			add {  if (!IsDisposed) { _backgroundWorker.RunWorkerCompleted += value; } }
 			remove {  if (!IsDisposed) { _backgroundWorker.RunWorkerCompleted -= value; } }
 		}
 
+		#endregion
+
+		#region Private Fields & Classes
+
+		private Stats _operationStats = null;
+		private Intersection _intersectionSet = null;
+		private BackgroundWorker _backgroundWorker = null;
+
+		private class Stats
+		{
+			public ulong Counter { get; internal set; }
+			public TimeSpan ProcessingTime { get; internal set; }
+			public Stats() { ProcessingTime = TimeSpan.Zero; }
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		#region Constructor
+
+		/// <summary>
+		///  Initializes a new instance of the AsyncBackgroundTask class.
+		/// </summary>		
 		public AsyncBackgroundTask(MainForm mainForm)
 		{
+			_operationStats = new Stats();
 			_backgroundWorker = new BackgroundWorker();
 
 			IsBusy = false;
 			IsDisposed = false;
 			CancellationPending = false;			
-			ArithmeticSequenceStats = new Stats();			
 		}
 
-		public void RunWorkerAsync()
+		#endregion
+
+		/// <summary>
+		/// Starts execution of a background operation.
+		/// </summary>
+		/// <returns>True if a new background operation was started, or false if a background operation was already running, was pending cancellation, or the object was disposed.</returns>
+		public bool RunWorkerAsync()
 		{
 			if (!CancellationPending && !IsBusy && !IsDisposed)
 			{
 				IsBusy = true;
-				this.DoWork += doWork;				
-				_backgroundWorker.RunWorkerAsync();				
+				this.DoWork += BeginDoWork;
+				_backgroundWorker.RunWorkerAsync();
+				return true;
 			}
+			return false;
 		}
 
-		public void CancelAsync()
+		/// <summary>
+		/// Requests cancellation of a pending background operation.
+		/// </summary>
+		/// <returns>True if the background worker was running and a cancellation request was sent, or false if no background operations were running, the operation had already been canceled, or the object was disposed.</returns>
+		public bool CancelAsync()
 		{
 			if (IsBusy && !CancellationPending && !IsDisposed)
 			{
 				CancellationPending = true;
-				if (IntersectionSet != null)
+				if (_intersectionSet != null)
 				{
-					IntersectionSet.CancelAsync();
+					_intersectionSet.CancelAsync();
+					return true;
 				}
 			}
+			return false;
 		}
 
+		/// <summary>
+		/// Releases all resources used by the class.
+		/// </summary>
 		public void Dispose()
 		{
 			if (!IsDisposed)
 			{
 				IsDisposed = true;
-				if (IntersectionSet != null)
+				if (_intersectionSet != null)
 				{
-					IntersectionSet.Dispose();
-					IntersectionSet = null;
+					if (!_intersectionSet.IsDisposed)
+					{
+						_intersectionSet.Dispose();
+					}
+					_intersectionSet = null;
 				}
 
 				if (_backgroundWorker != null)
@@ -83,30 +144,30 @@ namespace ThinqGUI
 					_backgroundWorker = null;
 				}
 
-				if (ArithmeticSequenceStats != null)
+				if (_operationStats != null)
 				{
-					ArithmeticSequenceStats = null;
+					_operationStats = null;
 				}
 			}
 		}
 
+		#endregion
+
 		#region Private Methods
-		//
-		// Private Methods
-		//
-		private void doWork(object sender, DoWorkEventArgs e)
+
+		private void BeginDoWork(object sender, DoWorkEventArgs e)
 		{
-			this.DoWork -= doWork;
+			this.DoWork -= BeginDoWork;
 			if (IsBusy && !CancellationPending && !IsDisposed)
 			{
-				this.RunWorkerCompleted += runWorkerCompleted;				
-				calculateIntersectionSet();
+				this.RunWorkerCompleted += BeginWorkerCompleted;
+				CalculateIntersectionSet();
 			}
 		}
 
-		private void runWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		private void BeginWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
 		{
-			this.RunWorkerCompleted -= runWorkerCompleted;
+			this.RunWorkerCompleted -= BeginWorkerCompleted;
 			Program.ThinqMainForm.SetControlsStatus(true);
 			if (CancellationPending)
 			{
@@ -115,23 +176,23 @@ namespace ThinqGUI
 			IsBusy = false;
 		}
 		
-		private void calculateIntersectionSet()
+		private void CalculateIntersectionSet()
 		{
-			if (!IsDisposed && !CancellationPending)
+			if (!IsDisposed && !CancellationPending && _intersectionSet == null)
 			{
 				DateTime startTime = DateTime.Now;
-				ulong minValue = Program.ThinqMainForm.StartValue;
-				ulong maxValue = Program.ThinqMainForm.MultipleMax;
+				ulong minValue = Program.ThinqMainForm.CoFactorMin;
+				ulong maxValue = Program.ThinqMainForm.CoFactorMax;
 				int padLen = maxValue.ToString().Length;
 				ulong[] cofactors = Program.ThinqMainForm.CoFactors.ToArray();
 
 				try
 				{
-					ArithmeticSequenceStats.Counter = 0;
-					IntersectionSet = new Intersection(minValue, maxValue, cofactors);
-					foreach (ulong factor in IntersectionSet.GetEnumerable())
+					_operationStats.Counter = 0;
+					_intersectionSet = new Intersection(minValue, maxValue, cofactors);
+					foreach (ulong factor in _intersectionSet.GetEnumerable())
 					{
-						ArithmeticSequenceStats.Counter++;
+						_operationStats.Counter++;
 						Program.DisplayFunction(string.Concat("{0,", padLen.ToString(), "}"), factor);
 
 						if (CancellationPending)
@@ -142,29 +203,20 @@ namespace ThinqGUI
 				}
 				finally
 				{
-					ArithmeticSequenceStats.ProcessingTime = DateTime.Now.Subtract(startTime);
+					_operationStats.ProcessingTime = DateTime.Now.Subtract(startTime);
 
 					StringBuilder strBldr = new StringBuilder();
 					strBldr.AppendLine("-------------------------------");
 					strBldr.AppendFormat("Max: {0:n0}", maxValue).AppendLine();
-					strBldr.AppendFormat("LCM[{0}]", string.Join(",", cofactors)); strBldr.AppendLine();
-					strBldr.AppendFormat("Factors found: {0}", ArithmeticSequenceStats.Counter); strBldr.AppendLine();
-					strBldr.AppendFormat("Time elapsed: {0}", ArithmeticSequenceStats.ProcessingTime.ToString(@"mm\:ss\.ff")); strBldr.AppendLine();
-					strBldr.AppendLine();
-					strBldr.AppendLine();
+					strBldr.AppendFormat("LCM[{0}]", string.Join(",", cofactors)).AppendLine();
+					strBldr.AppendFormat("Factors found: {0}", _operationStats.Counter).AppendLine();
+					strBldr.AppendFormat("Time elapsed: {0}", _operationStats.ProcessingTime.ToString(@"mm\:ss\.ff"));
+
 					Program.DisplayFunction(strBldr.ToString());
 				}
 			}
 		}
 
-		internal class Stats
-		{
-			public ulong Counter { get; internal set; }
-			public TimeSpan ProcessingTime { get; internal set; }
-			public Stats() { ProcessingTime = TimeSpan.Zero; }
-		}
-
 		#endregion
-
 	}
 }
