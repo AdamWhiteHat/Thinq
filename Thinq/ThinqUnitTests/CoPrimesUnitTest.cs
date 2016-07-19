ca6e47367742988f0178e61c09c53544a33169a5
Adam White <arakaska@gmail.com>
2016-07-19 03:36:48 -0600

    Added tests to exercise GetPrimeFactors

--- Thinq/ThinqUnitTests/CoPrimesUnitTest.cs
+++ Thinq/ThinqUnitTests/CoPrimesUnitTest.cs
@@ -34,7 +34,7 @@ namespace ThinqUnitTests
 		public void TestLcmTwo()
 		{
 			BigInteger[] testNumbers1 = new BigInteger[] { 251, 257 };
-			int expectedValue1 = 64507;
+			BigInteger expectedValue1 = 64507;
 
 			BigInteger resultValue1 = Coprimes.FindLCM(testNumbers1);
 
@@ -68,13 +68,13 @@ namespace ThinqUnitTests
 			BigInteger coprimeTo = 256;
 			BigInteger coprimeMin = 16777216;
 			BigInteger coprimeMax = 16777700;
-			BigInteger expectedCoprimeCount = 242;
+			int expectedCoprimeCount = 242;
 			BigInteger expectedFirst = 16777217;
 			BigInteger expectedLast = 16777699;
 
 			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
 			List<BigInteger> resultCoprimes = coprimes.GetCoprimes().ToList();
-			BigInteger resultCoprimeCount = resultCoprimes.Count;
+			int resultCoprimeCount = resultCoprimes.Count;
 
 			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
 			Assert.AreEqual(expectedFirst, resultCoprimes.First());
@@ -87,13 +87,13 @@ namespace ThinqUnitTests
 			BigInteger coprimeTo = 251;
 			BigInteger coprimeMin = 1234567000;
 			BigInteger coprimeMax = 1234568000;
-			BigInteger expectedCoprimeCount = 996;
+			int expectedCoprimeCount = 996;
 			BigInteger expectedFirst = 1234567000;
 			BigInteger expectedLast = 1234567999;
 
 			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
 			List<BigInteger> resultCoprimes = coprimes.GetCoprimes().ToList();
-			BigInteger resultCoprimeCount = resultCoprimes.Count;
+			int resultCoprimeCount = resultCoprimes.Count;
 
 			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
 			Assert.AreEqual(expectedFirst, resultCoprimes.First());
@@ -106,13 +106,70 @@ namespace ThinqUnitTests
 			BigInteger coprimeTo = 20160;
 			BigInteger coprimeMin = 6777122;
 			BigInteger coprimeMax = 6777131;
-			BigInteger expectedCoprimeCount = 0;
+			int expectedCoprimeCount = 0;
 
 			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
 			List<BigInteger> resultCoprimes = coprimes.GetCoprimes().ToList();
-			BigInteger resultCoprimeCount = resultCoprimes.Count;
+			int resultCoprimeCount = resultCoprimes.Count;
 
 			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+		}
+
+		[TestMethod]
+		public void TestFindPrimeFactors()
+		{
+			BigInteger coprimeTo = 111247819;
+			BigInteger coprimeMin = 2;
+			BigInteger coprimeMax = 111247820;
+			int expectedCoprimeCount = 2;
+			BigInteger primeP = 10007;
+			BigInteger primeQ = 11117;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<BigInteger> resultCoprimes = coprimes.GetPrimeFactors().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+			Assert.AreEqual(resultCoprimes[0], primeP);
+			Assert.AreEqual(resultCoprimes[1], primeQ);
+		}
+
+		[TestMethod]
+		public void TestFindPrimeFactors32bit()
+		{
+			BigInteger coprimeTo = 1202664026348236889;
+			BigInteger coprimeMin = 1069760585;
+			BigInteger coprimeMax = 1202664026348236890;
+			int expectedCoprimeCount = 2;								
+			BigInteger primeP = 1074936217;
+			BigInteger primeQ = 1118823617;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<BigInteger> resultCoprimes = coprimes.GetPrimeFactors().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+			Assert.AreEqual(resultCoprimes[0], primeP);
+			Assert.AreEqual(resultCoprimes[1], primeQ);
+		}
+
+		[TestMethod]
+		public void TestFindPrimeFactors64bit()
+		{
+			BigInteger coprimeTo = BigInteger.Parse("90459209396766660489526818707270239963");
+			BigInteger coprimeMin = 9223506588021000000;
+			BigInteger coprimeMax = BigInteger.Parse("90459209396766660489526818707270239964");
+			int expectedCoprimeCount = 2;
+			BigInteger primeP = 9223506588022268141;
+			BigInteger primeQ = 9807464062988563943;
+
+			Coprimes coprimes = new Coprimes(coprimeTo, coprimeMin, coprimeMax);
+			List<BigInteger> resultCoprimes = coprimes.GetPrimeFactors().ToList();
+			int resultCoprimeCount = resultCoprimes.Count;
+
+			Assert.AreEqual(expectedCoprimeCount, resultCoprimeCount);
+			Assert.AreEqual(resultCoprimes[0], primeP);
+			Assert.AreEqual(resultCoprimes[1], primeQ);
 		}
 	}
 }
